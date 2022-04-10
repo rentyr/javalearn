7c8d4c74834e61e5d3bdaa9c1697c546c476bbfe
Amritha Mohan <amrithamohan@Amrithas-MacBook-Air.local>
2022-04-10 13:29:47 +0530

    operators code

---
+++ src/First/Operators.java
@@ -0,0 +1,17 @@
+package First;
+
+public class Operators {
+
+	public static void main(String[] args) {
+
+		int number = 10;
+		number++;
+		System.out.println(number);
+
+		double doubleNumber = 23;
+
+		doubleNumber = doubleNumber / 12;
+		System.out.println(doubleNumber);
+
+	}
+}
