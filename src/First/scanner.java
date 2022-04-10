a63aa555921873aaeb1902df972add6b8eb6ceb5
Amritha Mohan <amrithamohan@Amrithas-MacBook-Air.local>
2022-04-10 12:36:14 +0530

    scaner code

--- src/First/scanner.java
+++ src/First/scanner.java
@@ -1,11 +1,23 @@
 package First;
+
 import java.util.Scanner;
 
 public class scanner {
 
 	public static void main(String[] args) {
-		
-	Scanner scan= new Scanner(System.in);
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = scan.nextLine();
+		System.out.println("Enter your age");
+		int age = scan.nextInt();
+		scan.nextLine();
+		System.out.println("Enter you job");
+		String job = scan.nextLine();
+		System.out.println("Name is " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Job:: " + job);
+
 	}
 
 }
